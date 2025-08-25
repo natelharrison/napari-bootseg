25a7c4b5d3dc26ea01a9f083aac44014a4030f29
Nate <78279182+natelharrison@users.noreply.github.com>
2025-08-25 15:18:26 -0700

    Moved image layer combo to toolpanel class in _widget.py

--- src/napari_bootseg/_widget.py
+++ src/napari_bootseg/_widget.py
@@ -16,28 +16,31 @@ class ToolPanel(Container):
         super().__init__(*children, **kwargs)
         self.viewer = viewer
 
+        self._image_layer_combo = create_widget(
+            label="Image", annotation="napari.layers.Image"
+        )
+        self.append(self._image_layer_combo)
+
+    def selected_image(self) -> Optional["napari.layers.Image"]:
+        layer = self._image_layer_combo.value
+        return layer
+
 class PredictLabels(ToolPanel):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__(viewer)
         
-        # image layer combo
-        self._image_layer_combo = create_widget(
-            label="Image", annotation="napari.layers.Image"
-        )
-
         # prediction button
         self._predict_btn = widgets.PushButton(text="Predict Labels")
         self._predict_btn.clicked.connect(self._predict)
 
         self.extend(
             [
-                self._image_layer_combo,
                 self._predict_btn
             ]
         )
     
     def _predict(self):
-        image_layer = self._image_layer_combo.value
+        image_layer = self.selected_image()
         print(type(image_layer))
         if image_layer is None:
             raise(ValueError("Select an image layer first."))
@@ -45,8 +48,8 @@ class PredictLabels(ToolPanel):
         prediction = predict_roi(image_layer.data)
         
         name = image_layer.name + "_labels"
-        if name in self._viewer.layers:
-            self._viewer.layers[name].data += prediction
+        if name in self.viewer.layers:
+            self.viewer.layers[name].data += prediction
         
-        self._viewer.add_labels(prediction, name=name)
+        self.viewer.add_labels(prediction, name=name)
         
