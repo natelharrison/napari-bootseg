c4f3fc67530f417ce8742053047eda26a2d3e819
Nate <78279182+natelharrison@users.noreply.github.com>
2025-08-24 21:43:59 -0700

    Removed auto generated comments and added core folder and files

--- src/napari_bootseg/_writer.py
+++ src/napari_bootseg/_writer.py
@@ -1,11 +1,3 @@
-"""
-This module is an example of a barebones writer plugin for napari.
-
-It implements the Writer specification.
-see: https://napari.org/stable/plugins/building_a_plugin/guides.html#writers
-
-Replace code below according to your needs.
-"""
 from __future__ import annotations
 
 from collections.abc import Sequence
